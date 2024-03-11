ad784f8b6a7a7c34e33b0459e8bbfec47441c5b8
AdamLuczaj <87152046+AdamLuczaj@users.noreply.github.com>
2024-03-11 18:49:09 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,42 @@
+COMP 3005 Winter 2024 Assignment 3 Question 1
+Student Name: Adam Luczaj
+Student #: 101196424
+
+Design decision:
+- I used psycopg3 because that is what were going to be using for the Final Project 1. Also, we are allowed to use any programming language for this assignment.
+- Database validation was not provided as in the brightspace discussion forum it was stated that we can assume all data will be of valid types and that this check doesn't have to be done.
+
+Functions:
+- getAllStudents: Function that gets all the students from the database
+- addStudents: Function that adds the student to the database
+- updateStudentEmail: Function that updates a student email based on a provided student_id
+- deleteStudent: Function that attempts to delete a student based on a provided student_id.
+
+Instructions to setup database for program runtime (using pgAdmin4 as discussed on brightspace discussion forum):
+1. Open pgAdmin4
+2. Open Servers Dropdown
+3. Open PostgreSQL 16 Dropdown
+4. Right click database and click Create->Database...
+5. Write a Database name (I used StudentManagementSystem) and press save.
+6. Right click your database name and press Query Tool.
+7. In this menu copy and paste:
+
+CREATE TABLE Student ( 
+student_id SERIAL PRIMARY KEY, 
+first_name TEXT NOT NULL, 
+last_name TEXT NOT NULL, 
+email TEXT NOT NULL UNIQUE, 
+enrollment_date DATE 
+); 
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES 
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'), 
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'), 
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
+
+8. Highlight all this text you pasted then press the execute button.
+9. In the comp3005a3q1AdamLuczaj.py file in the connect statement, fill out all the required system specific information detailed below the TODO (TA) comment.
+
+Instructions to run program:
+1. Install psycopg3 (do pip install psycopg then pip install "psycopg[binary]") (If that doesn't work please see this site for more information: https://www.psycopg.org/psycopg3/docs/basic/install.html)
+2. Run the program (In VSCode you can press the play button)
---
+++ comp3005a3q1AdamLuczaj.py
@@ -0,0 +1,87 @@
+import psycopg
+
+# Function that gets all the students from the database
+def getAllStudents(cursor):
+    cursor.execute("SELECT * FROM students")
+    studentRows = cursor.fetchall()
+
+    print("(student_id, first_name, last_name, email, enrollment_date)")
+
+    for studentRow in studentRows: 
+        
+        # Get each element to properly format it 
+        student_id = studentRow[0]
+        first_name = studentRow[1]
+        last_name = studentRow[2]
+        email = studentRow[3]
+        enrollment_date = studentRow[4]
+
+        # Format the row string to print it cleanly
+        studentRow = f"({student_id}, {first_name}, {last_name}, {email}, {enrollment_date})"
+
+        print(studentRow)
+
+# Function that adds the student to the database
+def addStudent(cursor, first_name, last_name, email, enrollment_date):
+    addSQL = f"INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ('{first_name}', '{last_name}', '{email}', '{enrollment_date}')"
+    cursor.execute(addSQL)
+
+# Function that updates a student email based on a provided student_id
+def updateStudentEmail(cursor, student_id, new_email):
+    updateSQL = f"UPDATE students set email = '{new_email}' WHERE students.student_id = '{student_id}'"
+    cursor.execute(updateSQL)
+
+# Function that attempts to delete a student based on a provided student_id.
+def deleteStudent(cursor, student_id):
+    deleteSQL = f"DELETE FROM students WHERE students.student_id = '{student_id}'" 
+    cursor.execute(deleteSQL)
+
+try:
+    
+    # Attempt to connect to the database
+    # TODO (TA): this will need to be modified according to the TA's database name, username, password, and port number. The only thing probably different for the TA may be the password they are using and the dbname when setting up with the instructions.
+    conn = psycopg.connect(dbname='StudentManagementSystem', user='postgres', password='postgres', host='localhost', port=5432)
+
+    # The cursor is used to make queries to the database/execute commands.
+    cursor = conn.cursor()
+
+    userChoice = ""
+
+    while userChoice != "exit":
+        userChoice = input("Please select your choice (getAllStudents, addStudent, updateStudentEmail, deleteStudent, exit): ")
+
+        if userChoice == "getAllStudents":
+            getAllStudents(cursor)
+
+        elif userChoice == "addStudent":
+            userFirstName = input("Please write the first name: ")
+            userLastName = input("Please write the last name: ")
+            userEmail = input("Please write the email: ")
+            userEnrollmentDate = input("Please write the enrollment date (YEAR-MONTH-DAY): ")
+            addStudent(cursor, userFirstName, userLastName, userEmail, userEnrollmentDate)
+
+        elif userChoice == "updateStudentEmail":
+            userStudentID = input("Please enter student id you wish to update: ")
+            userEmail = input("Please enter the email you wish to update: ")
+            updateStudentEmail(cursor, userStudentID, userEmail)
+
+        elif userChoice == "deleteStudent":
+            userStudentID = input("Please provide the id of the student you wish to remove: ")
+            deleteStudent(cursor, userStudentID)
+        
+        # Catch invalid selections 
+        elif userChoice != "exit":
+            print("Invalid choice selected. Please select from the listed choices.")
+
+# Catch any errors psycopg throws
+except psycopg.OperationalError as e:
+    error_message = f"Error: {e}"
+    print(error_message)
+    exit(1)
+
+# Close our database connections
+finally:
+    cursor.close()
+    conn.commit() # what this does is save the database changes.
+    conn.close()
+
